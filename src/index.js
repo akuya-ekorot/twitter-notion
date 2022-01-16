16ea5aa0b711e882f3f75d805513d7614b363a4b
Akuya Ekorot <akuyaekorot@gmail.com>
2022-01-16 21:04:21 +0300

    restarting this whole thing a fresh

--- src/index.js
+++ src/index.js
@@ -11,11 +11,12 @@ const config = {
   url: "https://api.twitter.com/oauth/request_token",
   headers: {
     "Content-Type": "application/x-www-form-urlencoded",
-    "Authorization": `OAuth oauth_consumer_key="${process.env.CONSUMER_KEY}", oauth_nonce="${uuid.v4()}", oauth_signature="oauth_signature", oauth_signature_method="HMAC-SHA1", oauth_timestamp="${Math.floor(Date.now()/1000)}", oauth_version="1.0"`
+    "Authorization": `OAuth oauth_consumer_key="${process.env.CONSUMER_KEY}", oauth_nonce="${uuid.v4()}", oauth_signature="oauth_signature", oauth_signature_method="HMAC-SHA1", oauth_timestamp="${Math.floor(Date.now() / 1000)}", oauth_version="1.0"`
   },
-  data: {
+  params: {
     "oauth_callback": new URL("http://localhost:3000/callback"),
-  }
+  },
+  "accept-encoding": ["gzip", "deflate", "br"]
 }
 
 axios(config).then((response) => {
